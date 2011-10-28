7fed191a61da62f95a655101208b00862fb19ec4
Michel Begoc <michel.begoc@gmail.com>
2011-10-27 22:23:25 -0400

    Isolation of the core framework

--- pmf/views/templates/content.tpl.php
+++
@@ -1,6 +0,0 @@
-<!-- the content template -->
-<?=$this->view->getMessages();?>
-<div style='background-color:#ccc;border:1px solid black;'>
-<h1><?=$this->data->getTitle();?></h1>
-<p><?=$this->data->getContent();?></p>
-</div>
